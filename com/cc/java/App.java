f8e4baa145085701680c5cee74656f052d88ca36
yoda <dlesnik23@gmail.com>
2022-04-14 12:05:24 +0200

    Flächenberechnung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,14 +3,31 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Triangle triangle = new Triangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
+
+        output("---- Flächen: --------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Dreieck: " + triangle.area());
+        output("Fläche Kreis: " + circle.area());
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
-
-
 }
-
